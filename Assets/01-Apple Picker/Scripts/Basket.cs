0cd3515a4a5ae0ed126dc5875d8fd360130bdf81
Sammy Abraham <s7abraham@gmail.com>
2023-09-19 18:24:33 -0400

    Updating the Basket script with scores (running into issues here)

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+
+    [Header("Set Dynamically")]
+
+    public Text scoreGT;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
