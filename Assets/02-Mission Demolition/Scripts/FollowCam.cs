faa1f1b8e11a6f89e2ce43364acd88d1ffa10a14
Sammy Abraham <s7abraham@gmail.com>
2023-09-26 18:02:23 -0400

    Running into Errors

---
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    static public GameObject POI;
+
+    [Header("Set Dynamically")]
+    public float camZ;
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if (POI == null)
+        {
+            return;
+        }
+    }
+
+    Vector3 destination = POI.transform.position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -9,7 +9,7 @@ public class Slingshot : MonoBehaviour
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
 
-    [Header("Set dynamically")]
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
 
     public Vector3 launchPos;
@@ -29,6 +29,34 @@ public class Slingshot : MonoBehaviour
     {
         //If slingshot is not in aimingMode, don't run this
         if (!aimingMode) return;
+
+        //Get the current mouse position in 2D screen coordinates
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Find the delta from the launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        //Limit mouseDelta to the radius of the Slingshot SphereCollider
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        //Move the projectile to this new position
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+        }
     }
 
     void OnMouseDown()
