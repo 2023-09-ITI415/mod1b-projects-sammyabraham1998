e93db7e2224187b8031bd29e66b5206ff94b279b
Sammy Abraham <s7abraham@gmail.com>
2023-09-26 16:49:25 -0400

    Wrote OnMouseDown() Method

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour
 {
 
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set dynamically")]
     public GameObject launchPoint;
+
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     private void Awake()
     {
         Transform launchPointTrans = transform.Find("Launch Point");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+
+    void OnMouseDown()
+    {
+        //The player has pressed the mouse button while over the slingshot
+        aimingMode = true;
+        //Instantiating a projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //Start it at the launchPoint
+        projectile.transform.position = launchPos;
+        //Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     // Start is called before the first frame update
