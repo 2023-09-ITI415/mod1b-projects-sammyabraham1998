7aca2528fb7381d212fa2d53d89a7d86b1f52e69
Sammy Abraham <s7abraham@gmail.com>
2023-09-28 10:28:40 -0400

    Fixed: Camera follows Projectile

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -7,6 +7,10 @@ public class FollowCam : MonoBehaviour
 
     static public GameObject POI;
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
     public float camZ;
 
@@ -21,8 +25,16 @@ public class FollowCam : MonoBehaviour
         {
             return;
         }
+
+        Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
+
     }
 
-    Vector3 destination = POI.transform.position;
 
 }
