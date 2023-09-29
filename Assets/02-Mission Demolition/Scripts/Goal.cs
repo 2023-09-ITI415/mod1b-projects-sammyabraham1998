3c563c0a31d5d382beebb427219d1c8921d0710f
Sammy Abraham <s7abraham@gmail.com>
2023-09-28 21:54:23 -0400

    Attached Goal Script to Goal Prefab (what was missing)

--- Assets/02-Mission Demolition/Scripts/Goal.cs
+++ Assets/02-Mission Demolition/Scripts/Goal.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-	static public bool 	goalMet = false;
+    static public bool goalMet = false;
+    void OnTriggerEnter(Collider other)
+    {
+        //	When	the	trigger	is	hit	by	something
+        //	Check	to	see	if	it's	a	Projectile
+        if (other.gameObject.tag == "Projectile")
+        {
+            //	If	so,	set	goalMet	to	true
+            Goal.goalMet = true;
+            //	Also	set	the	alpha	of	the	color	to	higher	opacity
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
 
-	void OnTriggerEnter(Collider other) {
-		// when the trigger is hit by something
-		// check to see if it's a Projectile 
-		if (other.gameObject.tag == "Projectile") {
-			// if so, set goalMet = true
-			Goal.goalMet = true;
-
-			// also set the alpha of the color of higher opacity
-			Material mat = GetComponent<Renderer>().material;
-			Color c = mat.color;
-			c.a = 1;
-			mat.color = c;
-		}
-	}
     // Start is called before the first frame update
     void Start()
     {
